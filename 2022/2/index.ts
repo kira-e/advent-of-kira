d501587130befbd8c9e378dd22247ae83b49fd41
Kira <119627945+kira-e@users.noreply.github.com>
2022-12-03 19:16:39 +0100

    3

--- 2022/2/index.ts
+++ 2022/2/index.ts
@@ -30,11 +30,10 @@ const shapesPoints = new Map([
 ]);
 
 const gameResultPoints = new Map([
-  [ Winner.DRAW, 3],
-  [ Winner.OPPONENT, 0],
-  [ Winner.PLAYER, 6],
+  [Winner.DRAW, 3],
+  [Winner.OPPONENT, 0],
+  [Winner.PLAYER, 6],
 ]);
-
 
 const loosingShapeMap = new Map([
   [Shape.ROCK, Shape.SCISSORS],
@@ -42,23 +41,27 @@ const loosingShapeMap = new Map([
   [Shape.SCISSORS, Shape.PAPER],
 ]);
 
-const winningShapeMap = new Map<Shape,Shape>(Array.from(loosingShapeMap.entries()).map(([shape1, shape2]: [Shape, Shape]) => [shape2, shape1]));
+const winningShapeMap = new Map<Shape, Shape>(
+  Array.from(loosingShapeMap.entries()).map(
+    ([shape1, shape2]: [Shape, Shape]) => [shape2, shape1]
+  )
+);
 
 const findStrategyShape = (opponent: Shape, playerCode: string) => {
   // X - need to lose
   // Y - need a draw
   // Z - need to win
-  if (playerCode ==='X') {
+  if (playerCode === "X") {
     return loosingShapeMap.get(opponent);
   }
-  if (playerCode === 'Y') {
+  if (playerCode === "Y") {
     return opponent;
-  } 
-  if (playerCode === 'Z') {
+  }
+  if (playerCode === "Z") {
     return winningShapeMap.get(opponent);
   }
   throw new Error(`Player code ${playerCode} is not supported`);
-}
+};
 
 const decideWinner = (opponent: Shape, player: Shape) => {
   if (opponent === player) {
@@ -74,42 +77,46 @@ const decideWinner = (opponent: Shape, player: Shape) => {
 
 const games = input
   .split("\n")
-  .map((row) => row.split(" ")
-    .map(shape => shape.trim()));
-
+  .map((row) => row.split(" ").map((shape) => shape.trim()));
 
 // task 1
-const points1 = games.map((shapes) => {
-  const elfShape = shapesMap.get(shapes[0]);
-  const playerShape = shapesMap.get(shapes[1]);
+const points1 = games
+  .map((shapes) => {
+    const elfShape = shapesMap.get(shapes[0]);
+    const playerShape = shapesMap.get(shapes[1]);
 
-  const winner = decideWinner(elfShape, playerShape);
-  
-  const playerPoints = shapesPoints.get(playerShape) + gameResultPoints.get(winner);
+    const winner = decideWinner(elfShape, playerShape);
 
-  // console.log(`>Elf: ${elfShape} vs Player: ${playerShape}`)
-  // console.log(`Game winner: ${winner}`);
-  // console.log(`Points earned: ${playerPoints}`);
-  
-  return playerPoints;
-}).reduce((prev, curr) => prev+curr);
+    const playerPoints =
+      shapesPoints.get(playerShape) + gameResultPoints.get(winner);
+
+    // console.log(`>Elf: ${elfShape} vs Player: ${playerShape}`)
+    // console.log(`Game winner: ${winner}`);
+    // console.log(`Points earned: ${playerPoints}`);
+
+    return playerPoints;
+  })
+  .reduce((prev, curr) => prev + curr);
 
 console.log(`Task1: Total player points: ${points1}`);
 
 // task 2
-const points2 = games.map((shapes) => {
-  const elfShape = shapesMap.get(shapes[0]);
-  const playerShape = findStrategyShape(elfShape, shapes[1]);
+const points2 = games
+  .map((shapes) => {
+    const elfShape = shapesMap.get(shapes[0]);
+    const playerShape = findStrategyShape(elfShape, shapes[1]);
 
-  const winner = decideWinner(elfShape, playerShape);
-  
-  const playerPoints = shapesPoints.get(playerShape) + gameResultPoints.get(winner);
+    const winner = decideWinner(elfShape, playerShape);
 
-  // console.log(`>Elf: ${elfShape} vs Player: ${shapes[1]} ${playerShape}`)
-  // console.log(`Game winner: ${winner}`);
-  // console.log(`Points earned: ${playerPoints}`);
-  
-  return playerPoints;
-}).reduce((prev, curr) => prev+curr);
+    const playerPoints =
+      shapesPoints.get(playerShape) + gameResultPoints.get(winner);
+
+    // console.log(`>Elf: ${elfShape} vs Player: ${shapes[1]} ${playerShape}`)
+    // console.log(`Game winner: ${winner}`);
+    // console.log(`Points earned: ${playerPoints}`);
+
+    return playerPoints;
+  })
+  .reduce((prev, curr) => prev + curr);
 
 console.log(`Task2: Total player points: ${points2}`);
---
+++ 2022/3/index.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import _ from "lodash";
+
+const input = fs.readFileSync("3/input", "utf-8");
+
+const rows = input.split("\n").map((row) => row.trim());
+
+// task 1
+const intersectionsInBags = rows
+  .map((row) => {
+    const bag1 = row.slice(0, row.length / 2);
+    const bag2 = row.slice(row.length / 2);
+
+    const bag1Set = new Set(bag1.split(""));
+    const bag2Set = new Set(bag2.split(""));
+
+    const setIntersections = [...bag1Set].filter((x) => bag2Set.has(x));
+    return setIntersections;
+  })
+  .flat();
+
+const A = "A".charCodeAt(0);
+const a = "a".charCodeAt(0);
+const z = "z".charCodeAt(0);
+const charDistance = z - a;
+
+const charToScore = (char: string) => {
+  const charCode = char.charCodeAt(0);
+  if (charCode >= a) {
+    // then lowercase - they have higher ascii value
+    return (a - charCode) * -1 + 1;
+  }
+  return (A - charCode) * -1 + 2 + charDistance;
+};
+
+const scoreSum = intersectionsInBags
+  .map((char) => charToScore(char))
+  .reduce((prev, curr) => prev + curr);
+
+console.log(`char distance = ${charDistance}`);
+console.log(`Task 1. Sum = ${scoreSum}`);
+
+// task 2
+const groupsScoreSum = _.chunk(rows, 3)
+  .flatMap((chunk) =>
+    _.intersection(...chunk.map((bag) => Array.from(new Set(bag.split("")))))
+  )
+  .map((intersectionChar) => charToScore(intersectionChar))
+  .reduce((prev, curr) => prev + curr);
+
+console.log(`Task 2. Groups' sum = ${groupsScoreSum}`);
